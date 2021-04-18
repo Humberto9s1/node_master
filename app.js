ce9bcda12f8c4735a2c09476af572470a144f4a8
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-04-18 04:01:43 -0300

    chat funcionando

--- app.js
+++ app.js
@@ -2,17 +2,52 @@
 var app = require('./config/server');
 
 /* Parametrizar a porta de escuta */
-var server = app.listen(80, function(){
+var server = app.listen(80, function () {
     console.log('Servidor ON');
 });
 
 var io = require('socket.io').listen(server);
 
+app.set('io', io);
+
 /* Criar a conexão por websocket */
-io.on('connection', function(socket) {
-   console.log('Usuário Conectou'); 
-   
-   socket.on('disconnect', function() {
-       console.log('Usuário Desconectou'); 
+io.on('connection', function (socket) {
+    console.log('Usuário Conectou');
+
+    socket.on('disconnect', function () {
+        console.log('Usuário Desconectou');
     });
+
+    /* diálogos */
+    socket.on('msgParaServidor', function (data) {
+        socket.emit('msgParaCliente',
+            {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        );
+
+        socket.broadcast.emit('msgParaCliente',
+            {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        );
+
+        /* participantes */
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit('participantesParaCliente',
+                {
+                    apelido: data.apelido
+                }
+            );
+
+            socket.broadcast.emit('participantesParaCliente',
+                {
+                    apelido: data.apelido
+                }
+            );
+        }
+
+    })
 });
