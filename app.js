71053eb7cce8b633a15aafe4f67961b1100a1fb7
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-04-18 02:22:44 -0300

    abrindo a conexao via websocket

--- app.js
+++ app.js
@@ -2,7 +2,17 @@
 var app = require('./config/server');
 
 /* Parametrizar a porta de escuta */
-app.listen(3000, function(){
+var server = app.listen(80, function(){
     console.log('Servidor ON');
 });
 
+var io = require('socket.io').listen(server);
+
+/* Criar a conexão por websocket */
+io.on('connection', function(socket) {
+   console.log('Usuário Conectou'); 
+   
+   socket.on('disconnect', function() {
+       console.log('Usuário Desconectou'); 
+    });
+});
